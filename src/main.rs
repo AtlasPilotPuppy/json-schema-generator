925d8bf7ded8baa1f44a6ff6d8ea2e6457a447d9
Anant <username@users.noreply.github.com>
2024-09-21 10:52:17 -0600

    now data can be input from stdin

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,6 @@ use std::path::Path;
 use json_schema_generator::generate_json_schema;
 use serde_json::Value;
 
-fn main() {
-    let (input, output) = parse_args().unwrap_or_else(|e| {
-        eprintln!("Error: {}", e);
-        std::process::exit(1);
-    });
-
-    let json_value = read_input(&input).unwrap_or_else(|e| {
-        eprintln!("Error reading input: {}", e);
-        std::process::exit(1);
-    });
-
-    let schema = generate_json_schema(&json_value);
-
-    write_output(&output, &schema).unwrap_or_else(|e| {
-        eprintln!("Error writing output: {}", e);
-        std::process::exit(1);
-    });
-}
-
 fn parse_args() -> Result<(Option<String>, Option<String>), String> {
     let args: Vec<String> = env::args().collect();
     let mut input = None;
@@ -37,18 +18,17 @@ fn parse_args() -> Result<(Option<String>, Option<String>), String> {
                     output = Some(args[i + 1].clone());
                     i += 2;
                 } else {
-                    return Err("Output file not specified after -o/--output".to_string());
+                    return Err("Missing argument for output file".to_string());
                 }
             }
-            arg if !arg.starts_with('-') => {
+            _ => {
                 if input.is_none() {
-                    input = Some(arg.to_string());
-                    i += 1;
+                    input = Some(args[i].clone());
                 } else {
-                    return Err("Multiple input files specified".to_string());
+                    return Err("Too many arguments".to_string());
                 }
+                i += 1;
             }
-            _ => return Err(format!("Unknown argument: {}", args[i])),
         }
     }
 
@@ -56,12 +36,13 @@ fn parse_args() -> Result<(Option<String>, Option<String>), String> {
 }
 
 fn read_input(input: &Option<String>) -> Result<Value, Box<dyn std::error::Error>> {
-    let json_str = if let Some(filename) = input {
-        fs::read_to_string(filename)?
-    } else {
-        let mut buffer = String::new();
-        io::stdin().read_to_string(&mut buffer)?;
-        buffer
+    let json_str = match input {
+        Some(filename) => fs::read_to_string(filename)?,
+        None => {
+            let mut buffer = String::new();
+            io::stdin().read_to_string(&mut buffer)?;
+            buffer
+        }
     };
 
     Ok(serde_json::from_str(&json_str)?)
@@ -76,4 +57,34 @@ fn write_output(output: &Option<String>, schema: &Value) -> Result<(), Box<dyn s
     }
 
     Ok(())
+}
+
+fn get_default_output(input: &Option<String>) -> Option<String> {
+    input.as_ref().map(|filename| {
+        let path = Path::new(filename);
+        let stem = path.file_stem().and_then(|s| s.to_str()).unwrap_or("output");
+        format!("{}.jsonschema", stem)
+    })
+}
+
+fn main() {
+    let (input, output) = parse_args().unwrap_or_else(|e| {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    });
+
+    let json_value = read_input(&input).unwrap_or_else(|e| {
+        eprintln!("Error reading input: {}", e);
+        std::process::exit(1);
+    });
+
+    let schema = generate_json_schema(&json_value);
+
+    let default_output = get_default_output(&input);
+    let final_output = output.or(default_output);
+
+    write_output(&final_output, &schema).unwrap_or_else(|e| {
+        eprintln!("Error writing output: {}", e);
+        std::process::exit(1);
+    });
 }
